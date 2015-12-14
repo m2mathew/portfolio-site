eaed20931be493c4234de1f630857395fa439c3e
Mike Mathew <m2mathew@me.com>
2015-12-14 01:25:24 -0600

    Success using modals in React

--- scripts/components/AboutComponent.js
+++ scripts/components/AboutComponent.js
@@ -1,14 +1,23 @@
 'use strict';
 
 let React = require('react');
+let Modal = require('react-bootstrap').Modal;
 
 module.exports = React.createClass({
+	getInitialState() {
+	   return { showModal: false };
+	},
+  	close() {
+    	this.setState({ showModal: false });
+  	},
+	open() {
+    	this.setState({ showModal: true });
+  	},
 	componentWillMount() {
 	    $('#myModal').on('shown.bs.modal', () => {
 			$('#myInput').focus()
 		});
 	},
-
 	render () {
 
 		return (
@@ -16,19 +25,19 @@ module.exports = React.createClass({
 				<div className="row text-justify">
 					<h3><strong>HOW TO MAKE A FRONT END DEVELOPER NAMED MIKE</strong> &bull; FALL IN LOVE WITH LEARNING &bull; GET A TRS-80 FROM UNCLE RICKY AND LEARN TO PROGRAM IN BASIC &bull; START HITTING THINGS WITH DRUMSTICKS &bull; PLAY LOTS OF VIDEO GAMES &bull; TAKE TWO YEARS OF COMPUTER SCIENCE IN HIGH SCHOOL USING C++ AND TURBO PASCAL &bull; SIT NEXT TO FUTURE WIFE IN COMPUTER SCIENCE &bull; MAJOR IN MUSIC EDUCATION IN COLLEGE &bull;
 
-						<a data-toggle="modal" href="#dci" class="text-callout">TOUR AND PERFORM WITH A DRUM AND BUGLE CORPS</a>
+						<a data-toggle="modal" href="#dci" className="text-callout">TOUR AND PERFORM WITH A DRUM AND BUGLE CORPS</a>
 
-                      <div class="modal fade" id="dci" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-                        <div class="modal-dialog">
-                          <div class="modal-content">
+                      <div className="modal fade" id="dci" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                        <div className="modal-dialog modal-lg">
+                          <div className="modal-content">
                             <div>
-                              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                              <button type="button" className="close" data-dismiss="modal" aria-hidden="true">&times;</button>
 
                             </div>
-                            <div class="modal-body">
-                              <img src="../../images/drum-corps-crown-2000.jpeg" alt="Mike on tour with Carolina Crown 2000"/>
+                            <div className="modal-body">
+                              <img className="modal-pic" src="../../images/drum-corps-crown-2000.jpeg" alt="Mike on tour with Carolina Crown 2000"/>
                             </div>
-                            <h4 class="modal-title">I marched with the Troopers in 1996 and Carolina Crown in 2000, both playing quads in the drum line. This experience is unlike anything else. We traveled on charter buses for a summer and stayed in high schools in each city. It is kind of like the army meets marching band.</h4>
+                            <h4 className="modal-title">I marched with the Troopers in 1996 and Carolina Crown in 2000, both playing quads in the drum line. This experience is unlike anything else. We traveled on charter buses for a summer and stayed in high schools in each city. It is kind of like the army meets marching band.</h4>
 
                           </div>
                         </div>
